4684d6543423dc705361a9a52885495d73ede8f5
KhaoulaAYARI <khawla.ayyari@gmail.com>
2024-09-27 12:44:46 +0200

    EvaluationFix et DetailCommandeFix sont ok

---
+++ src/DataFixtures/DetailCommandeFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Produit;
+use App\Entity\Commande;
+use App\Entity\DetailCommande;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class DetailCommandeFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        //obtenier tous les elements de coté1 "Commande"
+        $rep=$manager->getRepository(Commande::class);
+        $toutesLesCommandes=$rep->findAll();
+        //obtenier tous les elements de coté1 "Produit":
+        $rep=$manager->getRepository(Produit::class);
+        $tousLesProduits=$rep->findAll();
+
+        $faker=Factory::create("fr_BE");
+
+        for($i=0; $i<50; $i++){
+        $detailCommande = new DetailCommande(
+            [
+                'quantite'=>rand(5,100),
+                'prixUnitaire'=>$faker->randomFloat(2, 10, 100),
+            ]
+        );
+
+        //fixer l'element de l'entite de coté1 "Commande":
+        $detailCommande->setCommandesD($toutesLesCommandes[rand(0, count($toutesLesCommandes)-1)]);
+        $manager->persist($detailCommande);
+        //fixer l'element de l'entite de coté1 "Produit":
+        $detailCommande->setProduits($tousLesProduits[rand(0, count($tousLesProduits)-1)]);
+        $manager->persist($detailCommande);
+        } 
+
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [
+            CommandeFixtures::class,
+            ProduitsFixtures::class
+        ];
+    }
+}
---
+++ src/DataFixtures/EvaluationFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Evaluation;
+use App\Entity\Fournisseur;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class EvaluationFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        //obtenier tous les elements de coté1 "Fournisseur"
+        $rep=$manager->getRepository(Fournisseur::class);
+        $tousLesfournisseurs=$rep->findAll();
+        //obtenier tous les elements de coté1 "User"
+        $rep=$manager->getRepository(User::class);
+        $tousLesUser=$rep->findAll();
+
+
+        $faker=Factory::create("fr_BE");
+
+        for($i=0; $i<50; $i++){
+        $evaluation = new Evaluation(
+            [
+                'note'=>$faker->randomElement([1,2,3,4,5]),
+                'commentaire'=>$faker->sentence(5),
+                
+            ]
+        );
+
+        //fixer l'element de l'entite de coté1 "Fournisseur"
+        $evaluation->setEvaluationFournisseur($tousLesfournisseurs[rand(0, count($tousLesfournisseurs)-1)]);
+        //fixer l'element de l'entite de coté1 "User"
+        $evaluation->setUser($tousLesUser[rand(0, count($tousLesUser)-1)]);
+        $manager->persist($evaluation);
+
+        } 
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            FournisseurFixtures::class,
+            UserFixtures::class,
+        ];
+    }
+}
